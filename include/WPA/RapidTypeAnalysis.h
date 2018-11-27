4c98a6c1a964a2d63505605ae373d33097fb9e4f
Mohamad Barbar <mbarbar@runbox.com>
2018-11-27 21:26:27 +1100

    Add RapidTypeAnalysis algo. declarations.

--- include/WPA/RapidTypeAnalysis.h
+++ include/WPA/RapidTypeAnalysis.h
@@ -13,6 +13,14 @@
 #include "MemoryModel/PointerAnalysis.h"
 
 class RapidTypeAnalysis:  public BVDataPTAImpl{
+private:
+    std::set<const Function *> liveFunctions;  // F_L
+    std::set<const CallSite *> liveCallsites;  // S_L
+    std::set<std::string> liveClasses;         // C_L
+    /// Maps class names to callsite which may possibly be making calls to a
+    /// method within that class.
+    std::map<const std::string, const CallSite *> classToVCallMap;  // Q_V
+
 public:
     /// Constructor
     RapidTypeAnalysis(PTATY type = RapidTypeCPP_WPA)
@@ -46,6 +54,27 @@ public:
     static inline bool classof(const PointerAnalysis *pta) {
         return pta->getAnalysisTy() == RapidTypeCPP_WPA;
     }
+    //@}
+
+private:
+    /// Methods from the RTA algorithm in the thesis.
+    /// http://digitalassets.lib.berkeley.edu/techreports/ucb/text/CSD-98-1017.pdf page 61.
+    //@{
+    /// Returns true if a constructor call is used to build the base object of another.
+    static bool isBaseConstructorCall(const CallSite *cs) {
+        // TODO.
+    }
+
+    /// Entry into RTA.
+    void performRTA(SVFModule svfModule);
+    /// Analyzes all callsites in a function.
+    void analyzeFunction(const Function *fun, bool isBase);
+    /// Adds a callsite to the set of live callsites, and analyzes what it calls.
+    void addCall(const CallSite *cs);
+    /// Marks a class as active, and adds all relevant calls.
+    void instantiate(const std::string className);
+    /// Maps all possible classes (i.e. which the callsite can resolve to) to the callsite.
+    void addVirtualMappings(const CallSite *cs);
     //@}
 };
 
