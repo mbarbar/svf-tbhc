0484d623cd6e339a56a1fd16d6a53f40f9cd491c
Mohamad Barbar <mbarbar@runbox.com>
2018-11-28 19:22:46 +1100

    Implement instantiate for RTA.

--- lib/WPA/RapidTypeAnalysis.cpp
+++ lib/WPA/RapidTypeAnalysis.cpp
@@ -35,3 +35,34 @@ void RapidTypeAnalysis::performRTA(SVFModule svfModule) {
      analyzeFunction(svfModule.getFunction("main"), false);
 }
 
+void RapidTypeAnalysis::instantiate(const std::string className) {
+    if (liveClasses.find(className) != liveClasses.end()) {
+        // Class already instantiated; already live.
+        return;
+    }
+
+    // Set the class as live.
+    liveClasses.insert(className);
+
+    // Check if there are callsites to liven.
+    auto callsitesI = classToVCallsMap.find(className);
+    if (callsitesI == classToVCallsMap.end()) {
+        // Nothing to do, no callsites to be resolved.
+        return;
+    }
+
+    std::set<const CallSite *> callsites = callsitesI->second;
+
+    std::set<const CallSite *> callsitesToRemove;
+    for (auto csI = callsites.begin(); csI != callsites.end(); ++csI) {
+        if (liveCallsites.find(*csI) != liveCallsites.end()) {
+            addCall(*csI);
+            callsitesToRemove.insert(*csI);
+        }
+    }
+
+    // Remove callsites set to live for this class.
+    for (auto csI = callsitesToRemove.begin(); csI != callsitesToRemove.end(); ++csI) {
+        callsites.erase(*csI);
+    }
+}
