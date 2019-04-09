2e7f21f835593577c566ddb5d6879f366fe5e796
Mohamad Barbar <mbarbar@runbox.com>
2019-04-09 14:11:29 +1000

    TypeClone: add allocation rules.

--- lib/WPA/TypeClone.cpp
+++ lib/WPA/TypeClone.cpp
@@ -4,3 +4,32 @@
  *  Created on: Apr 09, 2019
  *      Author: Mohamad Barbar
  */
+
+#include "WPA/TypeClone.h"
+
+bool TypeClone::processAddr(const AddrSVFGNode* addr) {
+    double start = stat->getClk();
+
+    NodeID srcID = addr->getPAGSrcNodeID();
+    /// TODO: see FieldSensitive::processAddr
+    if (isFieldInsensitive(srcID)) {
+        srcID = getFIObjNode(srcID);
+    }
+
+    bool changed = addPts(addr->getPAGDstNodeID(), srcID);
+
+    // Should not have a type, not even undefined.
+    assert(idToTypeMap.find(srcID) == idToTypeMap.end() && "TypeClone: already has type!");
+    if (isHeapMemObj(srcID)) {
+        // Heap objects are initialised with no types.
+        idToTypeMap[srcID] = NULL;
+    } else {
+        idToTypeMap[srcID] = pag->getPAGNode(srcID)->getType();
+        assert(idToTypeMap[srcID] != NULL && "TypeClone: non-heap does not have a type?");
+    }
+
+    double end = stat->getClk();
+    addrTime += (end - start) / TIMEINTERVAL;
+
+    return changed;
+}
