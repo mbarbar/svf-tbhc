b2c72465d5c0e08cf34794fff838e0e362d0f121
Mohamad Barbar <mbarbar@runbox.com>
2019-08-29 17:05:14 +1000

    Refactor DCHG a little.

--- lib/MemoryModel/DCHG.cpp
+++ lib/MemoryModel/DCHG.cpp
@@ -9,6 +9,71 @@
 
 #include "MemoryModel/DCHG.h"
 
+void DCHGraph::handleDIBasicType(const DIBasicType *compositeType) {
+
+}
+
+void DCHGraph::handleDICompositeType(const DICompositeType *compositeType) {
+    switch (compositeType->getTag()) {
+    case llvm::dwarf::DW_TAG_array_type:
+        // TODO: add node likely 
+        break;
+    case llvm::dwarf::DW_TAG_class_type:
+    case llvm::dwarf::DW_TAG_structure_type:
+        break;
+    case llvm::dwarf::DW_TAG_union_type:
+        // TODO: unsure.
+        break;
+    case llvm::dwarf::DW_TAG_enumeration_type:
+        break;
+    default:
+        assert(false && "DCHGraph::buildCHG: unexpected CompositeType tag.");
+    }
+}
+
+void DCHGraph::handleDIDerivedType(const DIDerivedType *derivedType) {
+    switch (derivedType->getTag()) {
+    case llvm::dwarf::DW_TAG_inheritance:
+        // TODO: add inheritance edge.
+        break;
+    case llvm::dwarf::DW_TAG_member:
+        // TODO: don't care it seems.
+        break;
+    case llvm::dwarf::DW_TAG_typedef:
+        // TODO: perhaps attach it to the appropriate node.
+        break;
+    case llvm::dwarf::DW_TAG_pointer_type:
+        // TODO: add node likely.
+        break;
+    case llvm::dwarf::DW_TAG_ptr_to_member_type:
+        // TODO: add node likely 
+        break;
+    case llvm::dwarf::DW_TAG_reference_type:
+        // TODO: unsure.
+        break;
+    case llvm::dwarf::DW_TAG_const_type:
+        // TODO: unsure
+        break;
+    case llvm::dwarf::DW_TAG_atomic_type:
+        // TODO: unsure
+        break;
+    case llvm::dwarf::DW_TAG_volatile_type:
+        // TODO: unsure
+        break;
+    case llvm::dwarf::DW_TAG_restrict_type:
+        // TODO: unsure
+        break;
+    case llvm::dwarf::DW_TAG_friend:
+        // TODO: unsure.
+        break;
+    default:
+        assert(false && "DCHGraph::buildCHG: unexpected DerivedType tag.");
+    }
+}
+
+void DCHGraph::handleDISubroutineType(const DISubroutineType *subroutineType) {
+}
+
 void DCHGraph::buildCHG(void) {
     llvm::DebugInfoFinder finder;
     finder.processModule(module);
@@ -17,66 +82,13 @@ void DCHGraph::buildCHG(void) {
         llvm::DIType *type = *diTypeI;
 
         if (llvm::DIBasicType *basicType = SVFUtil::dyn_cast<llvm::DIBasicType>(type)) {
-            // TODO: add node likely.
+            handleDIBasicType(basicType);
         } else if (llvm::DICompositeType *compositeType = SVFUtil::dyn_cast<llvm::DICompositeType>(type)) {
-            switch (compositeType->getTag()) {
-            case llvm::dwarf::DW_TAG_array_type:
-                // TODO: add node likely 
-                break;
-            case llvm::dwarf::DW_TAG_class_type:
-                // TODO: add node.
-                break;
-            case llvm::dwarf::DW_TAG_structure_type:
-                // TODO: unsure.
-                break;
-            case llvm::dwarf::DW_TAG_union_type:
-                // TODO: unsure.
-                break;
-            case llvm::dwarf::DW_TAG_enumeration_type:
-                break;
-            default:
-                assert(false && "DCHGraph::buildCHG: unexpected CompositeType tag.");
-            }
+            handleDICompositeType(compositeType);
         } else if (llvm::DIDerivedType *derivedType = SVFUtil::dyn_cast<llvm::DIDerivedType>(type)) {
-            switch (derivedType->getTag()) {
-            case llvm::dwarf::DW_TAG_inheritance:
-                // TODO: add inheritance edge.
-                break;
-            case llvm::dwarf::DW_TAG_member:
-                // TODO: don't care it seems.
-                break;
-            case llvm::dwarf::DW_TAG_typedef:
-                // TODO: perhaps attach it to the appropriate node.
-                break;
-            case llvm::dwarf::DW_TAG_pointer_type:
-                // TODO: add node likely.
-                break;
-            case llvm::dwarf::DW_TAG_ptr_to_member_type:
-                // TODO: add node likely 
-                break;
-            case llvm::dwarf::DW_TAG_reference_type:
-                // TODO: unsure.
-                break;
-            case llvm::dwarf::DW_TAG_const_type:
-                // TODO: unsure
-                break;
-            case llvm::dwarf::DW_TAG_atomic_type:
-                // TODO: unsure
-                break;
-            case llvm::dwarf::DW_TAG_volatile_type:
-                // TODO: unsure
-                break;
-            case llvm::dwarf::DW_TAG_restrict_type:
-                // TODO: unsure
-                break;
-            case llvm::dwarf::DW_TAG_friend:
-                // TODO: unsure.
-                break;
-            default:
-                assert(false && "DCHGraph::buildCHG: unexpected DerivedType tag.");
-            }
+            handleDIDerivedType(derivedType);
         } else if (llvm::DISubroutineType *subroutineType = SVFUtil::dyn_cast<llvm::DISubroutineType>(type)) {
-            // TODO: unsure.
+            handleDIDerivedType(subroutineType);
         } else {
             assert(false && "DCHGraph::buildCHG: unexpected DIType.");
         }
